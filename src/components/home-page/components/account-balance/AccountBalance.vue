974f9b201df4976c5daa41fa0ff2d8bf05f204d7
Joshua Genereux <jgenereux@stratacache.com>
2025-01-13 13:04:59 -0500

    working on blance history print out

--- src/components/home-page/components/account-balance/AccountBalance.vue
+++ src/components/home-page/components/account-balance/AccountBalance.vue
@@ -12,17 +12,28 @@ export default defineComponent({
   setup() {
     const balanceStore = useBalanceStore();
     const currentBalance = computed(() => balanceStore.accountBalance);
+    const balanceHistory = computed(() => balanceStore.balanceHistory);
 
     console.log(balanceStore.accountBalance);
 
-    return { currentBalance };
+    return { currentBalance, balanceHistory };
   },
 
   render() {
     return (
       <div class="account-balance">
-        <div class="header">Account Balance</div>
-        <div class="balance">{this.currentBalance}</div>
+        <div class="header">Account Balance History</div>
+        <div class="balances-container">
+          <ul class="balance-history">
+            {this.balanceHistory.map((entry, index) => (
+              <li key="index" class="balance-line">
+                <div class="name">{entry.name}</div>
+                <div class="type">{entry.type}</div>
+                <div class="amount">{entry.amount}</div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   },
@@ -34,6 +45,7 @@ export default defineComponent({
   width: 450px;
   height: 200px;
   border: 1px solid white;
+  box-sizing: border-box;
 
   .header {
     width: 100%;
@@ -43,8 +55,21 @@ export default defineComponent({
     border-bottom: 1px solid white;
   }
 
-  .balance {
-    color: white;
+  .balances-container {
+    width: 100%;
+    height: 85%;
+    padding: 10px 10px;
+
+    .balance-history {
+      list-style-type: none;
+      padding: 0;
+
+      .balance-line {
+        display: flex;
+        justify-content: space-around;
+        color: white;
+      }
+    }
   }
 }
 </style>
